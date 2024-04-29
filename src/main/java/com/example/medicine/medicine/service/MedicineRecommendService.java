6a334a93476ed78a515ceaa2a2f7435f926aa269
Jung-ji-hyeon <ejxldortm@naver.com>
2024-04-29 20:55:21 +0900

    kakao 지도 api를 통해 로드뷰와 길안내 url 만들기

--- src/main/java/com/example/medicine/medicine/service/MedicineRecommendService.java
+++ src/main/java/com/example/medicine/medicine/service/MedicineRecommendService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,6 +24,9 @@ public class MedicineRecommendService {
 
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
+
+    private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
     public List<OutputDto> recommendMedicineList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
@@ -44,12 +48,20 @@ public class MedicineRecommendService {
     }
 
     private OutputDto convertToOutputDto(Direction direction) {
+
+        String params = String.join(",", direction.getTargetMedicineName(),
+                String.valueOf(direction.getTargetLatitude()), String.valueOf(direction.getTargetLongitude()));
+
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params).toUriString();
+
+        log.info("direction params : {}, url : {}", params, result);
+
         return OutputDto.builder()
                 .medicineName(direction.getTargetMedicineName())
                 .medicineAddress(direction.getTargetAddress())
                 .medicineNumber(direction.getTargetMedicineNumber())
-                .directionUrl("")
-                .roadViewUrl("")
+                .directionUrl(result)
+                .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
                 .distance(String.format("%.2f KM", direction.getDistance()))
                 .build();
     }
